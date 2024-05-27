562627902508e280f4c09938abcc1f550ab2159a
qkiet2k4 <104260937+qkiet1002@users.noreply.github.com>
2024-05-28 00:39:12 +0700

    Fix Multi

--- Assets/Scripts/Gun/gun.cs
+++ Assets/Scripts/Gun/gun.cs
@@ -1,106 +1,62 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using Invector.vCharacterController;
-using Unity.Netcode;
 
-public class Gun : NetworkBehaviour
+public class SimpleGun : MonoBehaviour
 {
     [Header("Gun Settings")]
-    public int damage;
-    public float timeBetweenShooting, spread, range, reloadTime, timeBetweenShots;
-    public int magazineSize, bulletsPerTap;
-    public bool allowButtonHold;
+    public int damage = 10;
+    public float timeBetweenShooting = 0.5f;
+    public int magazineSize = 30;
+    public float reloadTime = 2f;
+    public bool allowButtonHold = true;
 
-    private int bulletsLeft, bulletsShot;
-    private bool shooting, readyToShoot, reloading;
+    private int bulletsLeft;
+    private bool readyToShoot = true;
+    private bool reloading;
 
     [Header("References")]
     public Camera fpsCam;
     public Transform attackPoint;
-    public LayerMask whatIsEnemy;
-    public LayerMask whatIsRig;
-    public GameObject bulletHoleGraphicEnemy;
-    public GameObject bulletHoleGraphicRig;
-    public GameObject muzzleFlash;
     public GameObject bulletPrefab;
     public TextMeshProUGUI ammoText;
     public TextMeshProUGUI outOfAmmoText;
 
-    private vThirdPersonController thirdPersonController;
-
     private void Start()
     {
-        if (IsOwner)
-        {
-            InitializeGun();
-        }
+        InitializeGun();
     }
 
     private void InitializeGun()
     {
         bulletsLeft = magazineSize;
-        readyToShoot = true;
         outOfAmmoText.gameObject.SetActive(false);
-
-        // Find the local player object managed by Netcode
-        var localPlayer = NetworkManager.Singleton.LocalClient.PlayerObject;
-        Debug.Log(localPlayer);
-
-        if (localPlayer != null)
-        {
-            thirdPersonController = localPlayer.GetComponentInChildren<vThirdPersonController>();
-            if (thirdPersonController == null)
-            {
-                Debug.LogError("vThirdPersonController component not found on the local player.");
-            }
-        }
-        else
-        {
-            Debug.LogError("Local player object not found.");
-        }
     }
 
     private void Update()
     {
-        if (!IsOwner) return;
-
         HandleInput();
         ammoText.SetText(bulletsLeft + "/" + magazineSize);
 
         if (bulletsLeft == 0 && !reloading)
         {
             outOfAmmoText.gameObject.SetActive(true);
-            //StartCoroutine(HideOutOfAmmoTextAfterDelay(5f));
         }
-    }
-
-    private IEnumerator HideOutOfAmmoTextAfterDelay(float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        outOfAmmoText.gameObject.SetActive(false);
     }
 
     private void HandleInput()
     {
-        if (thirdPersonController == null) return;
-
-        // Handle movement input
-        Vector3 movementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-        thirdPersonController.UpdateMovementState(movementInput);
-
-        // Handle shooting input
-        shooting = allowButtonHold ? Input.GetKey(KeyCode.Mouse1) : Input.GetKeyDown(KeyCode.Mouse1);
+        if (allowButtonHold ? Input.GetKey(KeyCode.Mouse1) : Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            if (readyToShoot && !reloading && bulletsLeft > 0)
+            {
+                Shoot();
+            }
+        }
 
         if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading)
-            Reload();
-
-        // Shoot if ready and shooting
-        if (readyToShoot && shooting && !reloading && bulletsLeft > 0)
         {
-            bulletsShot = bulletsPerTap;
-            Shoot();
+            Reload();
         }
     }
 
@@ -108,29 +64,19 @@ public class Gun : NetworkBehaviour
     {
         readyToShoot = false;
 
-        float x = Random.Range(-spread, spread);
-        float y = Random.Range(-spread, spread);
-        Vector3 direction = fpsCam.transform.forward + new Vector3(x, y, 0);
-
-        GameObject bullet = Instantiate(bulletPrefab, attackPoint.position, Quaternion.LookRotation(direction));
+        // Instantiate bullet
+        GameObject bullet = Instantiate(bulletPrefab, attackPoint.position, fpsCam.transform.rotation);
         Bullet bulletScript = bullet.GetComponent<Bullet>();
         bulletScript.damage = damage;
-        bulletScript.whatIsEnemy = whatIsEnemy;
-        bulletScript.whatIsRig = whatIsRig;
-        bulletScript.bulletHoleGraphicEnemy = bulletHoleGraphicEnemy;
-        bulletScript.bulletHoleGraphicRig = bulletHoleGraphicRig;
-
-        Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity);
-        Destroy(bullet, 10f);
 
         bulletsLeft--;
-        bulletsShot--;
-
-        thirdPersonController.RotateToCameraDirection(fpsCam.transform.forward);
 
         Invoke("ResetShot", timeBetweenShooting);
-        if (bulletsShot > 0 && bulletsLeft > 0)
-            Invoke("Shoot", timeBetweenShots);
+
+        if (bulletsLeft == 0 && !reloading)
+        {
+            outOfAmmoText.gameObject.SetActive(true);
+        }
     }
 
     private void ResetShot()
@@ -141,8 +87,8 @@ public class Gun : NetworkBehaviour
     private void Reload()
     {
         reloading = true;
-        Invoke("ReloadFinished", reloadTime);
         outOfAmmoText.gameObject.SetActive(false);
+        Invoke("ReloadFinished", reloadTime);
     }
 
     private void ReloadFinished()
